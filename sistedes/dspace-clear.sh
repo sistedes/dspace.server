c4c5c00a508041b89d2ff1334fc5b3b31a7bd4dd
Abel <abgolla@gmail.com>
2023-05-11 19:44:10 +0200

    Add Sistedes registry metadata

--- sistedes/dspace-clear.sh
+++ sistedes/dspace-clear.sh
@@ -10,4 +10,5 @@ rm -rf $server/assetstore/*
 /opt/dspace-fix-permissions.sh
 /opt/dspace-create-admin.sh
 "$server/bin/dspace" initialize-entities -f "$server/config/entities/relationship-types.xml"
+"$server/bin/dspace" dsrun org.dspace.administer.MetadataImporter -f "$server/config/registries/sistedes-types.xml"
 systemctl start tomcat9.service
