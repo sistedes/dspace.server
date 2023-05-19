a4aebf7f9f5b03878d2f7a927cff01001ae36a97
Abel <abgolla@gmail.com>
2023-05-19 04:49:04 +0200

    Reorder command

--- sistedes/dspace-clear.sh
+++ sistedes/dspace-clear.sh
@@ -8,7 +8,7 @@ yes | $server/bin/dspace database clean
 sudo -u postgres psql --username=postgres dspace -c "CREATE EXTENSION pgcrypto;"
 rm -rf $server/assetstore/*
 /opt/dspace-fix-permissions.sh
+"$server/bin/dspace" initialize-entities -f "$server/config/entities/sistedes-relationship.xml"
 /opt/dspace-create-admin.sh
 systemctl start tomcat9.service
-"$server/bin/dspace" initialize-entities -f "$server/config/entities/sistedes-relationship.xml"
 
